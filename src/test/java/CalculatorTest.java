205f15286f4421984a816f0e40b75ac2311890a4
MatiuninaMarina <aprelkina.marina@mail.ru>
2023-04-29 18:17:34 +0400

    Tests added and mistakes corrected

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -20,4 +20,32 @@ public class CalculatorTest {
         assertThat(clothes.getSum()).isEqualTo(maxCategory.getSum());
         assertThat(clothes.getCategory()).isEqualTo(maxCategory.getCategory());
     }
+
+    @Test
+    public void recalculateSums() {
+        MaxCategoryCalculator maxCategoryCalculator = new MaxCategoryCalculator();
+        Map<String, Integer> map = new HashMap<>();
+        map.put("еда", 500);
+        map.put("одежда", 2000);
+        map.put("быт", 300);
+        Map<String, String> categories = new HashMap<>();
+        categories.put("булка", "еда");
+        categories.put("шапка", "одежда");
+        categories.put("мыло", "быт");
+
+        maxCategoryCalculator.recalculateSums("булка", 1600, map, categories);
+
+        assertThat(map.get("еда")).isEqualTo(2100);
+        assertThat(map.get("одежда")).isEqualTo(2000);
+        assertThat(map.get("быт")).isEqualTo(300);
+        assertThat(map.size()).isEqualTo(3);
+
+        maxCategoryCalculator.recalculateSums("брюки", 1600, map, categories);
+
+        assertThat(map.get("еда")).isEqualTo(2100);
+        assertThat(map.get("одежда")).isEqualTo(2000);
+        assertThat(map.get("быт")).isEqualTo(300);
+        assertThat(map.get("другое")).isEqualTo(1600);
+        assertThat(map.size()).isEqualTo(4);
+    }
 }
